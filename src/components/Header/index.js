7c07858a99225a09a1c7a2afa19e8d22639dd528
enzo <enzoserafin@gmail.com>
2021-05-03 23:52:11 -0300

    styled-components; css global no html; import assets; header compoent;

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import * as S from './styles'
+import logo from '../../assets/logo.png'
+import bell from '../../assets/bell.png'
+
+function Header() {
+    return (
+        <S.Container>
+            <S.LeftSide>
+                <img src={logo} alt="Logo" />
+
+            </S.LeftSide>
+            <S.RightSide>
+                <a href="www.google.com.br">INÍCIO</a>
+                <span className="dividir" />
+                <a href="www.google.com.br">NOVA TAREFA</a>
+                <span className="dividir" />
+                <a href="www.google.com.br">SINCRONIZAR CELULAR</a>
+                <span className="dividir" />
+                <a href="www.google.com.br" id="notification">
+                    <img src={bell} alt="Notificação" />
+                    <span>5</span>
+                </a>
+            </S.RightSide>
+        </S.Container>
+    )
+}
+
+export default Header
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import * as S from './styles'
+
+import Header from '../../components/Header'
 
 function Home() {
-    return <h1>Bem vindo a home</h1>
+    return (
+        <S.Container>
+            <Header />
+        </S.Container>
+    )
 }
 
 export default Home;
