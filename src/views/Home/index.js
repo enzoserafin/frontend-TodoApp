85296061871cc9f469904176b9b8f94afd8806ce
enzo <enzoserafin@gmail.com>
2021-05-03 23:54:45 -0300

    editorconfig

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -4,11 +4,11 @@ import * as S from './styles'
 import Header from '../../components/Header'
 
 function Home() {
-    return (
-        <S.Container>
-            <Header />
-        </S.Container>
-    )
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  )
 }
 
 export default Home;
