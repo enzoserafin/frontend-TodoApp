9c65d5ac52e10bc184cf6a15203ca61fe914f336
enzo <enzoserafin@gmail.com>
2021-05-05 22:04:06 -0300

    footer component; import filter icont; filter card component;

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+// eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
   width: 100%;
+`;
+
+export const FilterArea = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px;
 `;
